7fe6b65d3a83181e72d687887e4bfe628db22057
Kartik <kartik.a.gala@gmail.com>
2021-12-30 15:52:56 +0530

    commit

--- moving_rectangle/moving_rectangle.js
+++ moving_rectangle/moving_rectangle.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
   background(100);
-  fill(60, 60, 200);
+  //fill(60, 60, 200);
   strokeWeight(5);
   rectMode(CENTER); // to ditermine the center of the rectangle.
   rect (mouseX, mouseY, 50, 50, 10); // creates an interactive rectangle with moving mouse.
