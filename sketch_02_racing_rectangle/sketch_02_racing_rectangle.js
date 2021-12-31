1a658bf811fb73fff8601514502d1d5d0517e0b2
Kartik <kartik.a.gala@gmail.com>
2021-12-31 16:35:10 +0530

    commit

---
+++ sketch_02_racing_rectangle/sketch_02_racing_rectangle.js
@@ -0,0 +1,24 @@
+let song;
+
+function preload() {
+  song = loadSound('/assets/Electric_Ten_Broke_in_Summer.mp3');
+}
+
+function setup() {
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(canvasPressed);
+  background(220);
+  text('tap here to play', 10, 20);
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  song.play();
+}
+
+// Chrome 70 will require user gestures to enable web audio api > https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+// Click on the web page to start audio
+function touchStarted() {
+  getAudioContext().resume();
+}
---
+++ sketch_02_racing_rectangle2/sketch_02_racing_rectangle2.js
@@ -0,0 +1,42 @@
+//let song;
+
+//function preload() {
+//  song = loadSound('/assets/Sat_Narayan_short_Edit_1.mp3');
+//}
+
+let song, buttton, amp;
+let volHistory = [];
+
+function preload() {
+  song = loadSound("/assets/Sat_Narayan_short_Edit_1.mp3");
+  getAudioContext().resume();
+}
+
+function setup() {
+  createCanvas(600, 600);
+  song.play();
+  amp = new p5.Amplitude();
+}
+
+function draw() {
+  background(0);
+  let vol = amp.getLevel();
+  volHistory.push(vol);
+  for (let x = 0; x < volHistory.length; x++) {
+    stroke(255);
+    let y = map(volHistory[x], 0, 1, height, 0);
+    point(x, y);
+  }
+  // ellipse(300, 300, vol*300, vol*300);
+  console.log(vol);
+}
+
+function mousePressed() {
+  userStartAudio();
+}
+
+// Chrome 70 will require user gestures to enable web audio api > https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+// Click on the web page to start audio
+function touchStarted() {
+  getAudioContext().resume();
+}
