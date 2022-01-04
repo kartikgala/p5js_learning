616e124fe7579415dd9722e577037a79ec18e5b9
Kartik <kartik.a.gala@gmail.com>
2022-01-04 23:50:33 +0530

    commit

--- 02_circles_02/02_circles_02.js
+++ 02_circles_02/02_circles_02.js
@@ -2,16 +2,14 @@ var circleX = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background('#999');
 }
 
 function draw() {
-  
-  circle (circleX, 300, 100 ,100);
-  circleX ++;
+  circle(circleX, 300, 100, 100);
+  circleX++;
 
-  if (mouseX == circleX){
+  if (mouseX == circleX) {
     fill(255, 123, 255);
-  }
-    else(255);
-  }
+  } else 255;
+}
