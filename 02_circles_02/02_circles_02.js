5944d2e33c113b3c4f045a7642aa4370d60eac48
Kartik <kartik.a.gala@gmail.com>
2022-01-05 07:27:26 +0530

    commit

--- 02_circles_02/02_circles_02.js
+++ 02_circles_02/02_circles_02.js
@@ -1,19 +1,47 @@
 var circleX = 0;
 var circleY = 0;
+let rad = 60; // Width of the shape
+let xpos, ypos; // Starting position of shape
+let xspeed = 5; // Speed of the shape
+let yspeed = 6; // Speed of the shape
 
+let xdirection = 1; // Left or Right
+let ydirection = 1; // Top to Bottom
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background(220);
+  background(220);
+  // noStroke();
+  strokeWeight(5);
+  frameRate(30);
+  ellipseMode(RADIUS);
+  // Set the starting position of the shape
+  xpos = width / 2;
+  ypos = height / 2;
 }
 
 function draw() {
-  background(120);
-  ellipse(circleY, 300, 100, 100);
-  circle++;
+  background(50);
+  smooth(1);
 
-  if (mouseX >= circleY - 50 && mouseX <= circleY + 50) {
-    fill(255, 123, 255);
+  // Update the position of the shape
+  xpos = xpos + xspeed * xdirection;
+  ypos = ypos + yspeed * ydirection;
+
+  // Test to see if the shape exceeds the boundaries of the screen
+  // If it does, reverse its direction by multiplying by -1
+  if (xpos > width - rad || xpos < rad) {
+    xdirection *= -1;
+  }
+  if (ypos > height - rad || ypos < rad) {
+    ydirection *= -1;
+  }
+
+  // Draw the shape
+  ellipse(xpos, ypos, rad, rad);
+
+  if (mouseX >= xpos-50 && mouseY <= ypos+50 && mouseY) {
+fill(random(225), random(255), random(255), 100);    
   } else {
-    fill(255);
+    fill(random(50));    
   }
 }
